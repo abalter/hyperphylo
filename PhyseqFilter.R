b0f5f1c1dab429511c0600ec3505dccfac974653
Ariel Balter <ariel@arielbalter.com>
2018-11-04 19:44:41 -0800

    pulled out writetaxglomtable

--- PhyseqFilter.R
+++ PhyseqFilter.R
@@ -314,38 +314,38 @@ where
 #   print(sampleIDs)
 #   study_metadata$SampleID = unlist(sampleIDs)
 # 
-#   
+# 
 #   sample_id_string = paste0(paste0("sum(", sampleIDs, ") as ", sampleIDs, collapse=",\n  ") )
 #   # sample_id_string = paste0(paste0(sampleIDs,collapse=",\n  ") )
-#   
+# 
 #   SQL = "
-# select 
+# select
 #   Phylum||'_'||Genus as Taxa,
 #   %s
-# from 
-#   asv_table inner join taxa_table 
+# from
+#   asv_table inner join taxa_table
 # on
 #   taxa_table.row_names == asv_table.row_names
 # group by Taxa
 #   "
-#       
+# 
 #   SQL = sprintf(SQL, sample_id_string)
-#   
+# 
 #   cat(SQL)
-#   
+# 
 #   temp = sqldf(row.names=T, SQL)
 #   print(str(temp))
-#   
+# 
 #   temp[sampleIDs] = apply(temp[sampleIDs], 2, function(col){col/sum(col)})
 #   print(head(temp))
-#   
+# 
 #   print(colSums(temp[sampleIDs]))
-#   
+# 
 #   taxa_abundance_table = temp
-#   
+# 
 #   write.xlsx2(
-#     taxa_abundance_table, 
-#     file=filename, 
+#     taxa_abundance_table,
+#     file=filename,
 #     sheetName=sheetname,
 #     append=T,
 #     col.names=T,
